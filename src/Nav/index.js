9f4810ef9aabeb30f4a0a598f903718a0f34947f
Jonathan Huynh <jonhuynh05@gmail.com>
2019-12-16 23:57:34 -0800

    add onclick

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -26,27 +26,47 @@ class Nav extends Component{
                     <div className="nav-div" id="name">
                         <Link smooth={true} offset={-70} className="nav-a" id="nav-a-name" to="jonathan-huynh">Jonathan Huynh</Link>
                     </div>
-                    <div id="hamburger-stack">
-                        <div className="hamburger-bars"></div>
-                        <div className="hamburger-bars"></div>
-                        <div className="hamburger-bars"></div>
-                    </div>
+                    {
+                        this.state.open
+                        ?
+                        <div id="hamburger-stack" onClick={this.handleClose}>
+                            <div className="hamburger-bars"></div>
+                            <div className="hamburger-bars"></div>
+                            <div className="hamburger-bars"></div>
+                        </div>
+                        :
+                        <div id="hamburger-stack" onClick={this.handleOpen}>
+                            <div className="hamburger-bars"></div>
+                            <div className="hamburger-bars"></div>
+                            <div className="hamburger-bars"></div>
+                        </div>
+                    }
+
                     {/* <div id="hamburger-container"> */}
-                    <div className="nav-row">
-                        <div className="nav-div" id="nav-skills">
-                            <Link smooth={true} offset={-70} className="nav-a" to="skills">Skills</Link>
+                   {
+                       this.state.open
+                       ?
+                       <>
+                       <div className="nav-row">
+                            <div className="nav-div" id="nav-skills">
+                                <Link smooth={true} offset={-70} className="nav-a" to="skills">Skills</Link>
+                            </div>
                         </div>
-                    </div>
-                    <div className="nav-row">
-                        <div className="nav-div" id="nav-portolio">
-                            <Link smooth={true} offset={-70} className="nav-a" to="portfolio">Portfolio</Link>
+                        <div className="nav-row">
+                            <div className="nav-div" id="nav-portolio">
+                                <Link smooth={true} offset={-70} className="nav-a" to="portfolio">Portfolio</Link>
+                            </div>
                         </div>
-                    </div>
-                    <div className="nav-row">
-                        <div className="nav-div" id="nav-contact">
-                            <Link smooth={true} offset={-70} className="nav-a" to="contact">Contact</Link>
+                        <div className="nav-row">
+                            <div className="nav-div" id="nav-contact">
+                                <Link smooth={true} offset={-70} className="nav-a" to="contact">Contact</Link>
+                            </div>
                         </div>
-                    </div>
+                        </>
+                        :
+                        null
+                   }
+
                 </div>
                 {/* </div> */}
             </nav>
